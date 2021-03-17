e6e43ed9cd286b576f9dcca143e06867daa4a448
uka151 <umeshagrahari87@gmail.com>
2021-03-18 00:13:53 +0530

    Update features

--- src/Component/LineChart.js
+++ src/Component/LineChart.js
@@ -4,38 +4,38 @@ import {Line} from 'react-chartjs-2';
 function LineChart(){
     //base chart data before render
      const [chartdata, setChartData]=useState({});
-     const [data,setData]=useState([12,38,15,22,30,18,60]);
+     const [data,setData]=useState([12,38,15,22,30,18,60
+    ]);
      const [labal, setLabal]=useState(['Jan', 'Feb','Mar','Apr','May','Jun',
      'Jul']);
      // Hold the data after delete operation perform
-     const [chartEntries, setchartEntries] = useState([]);
+     const [chartEntries, setchartEntries] = useState({});
      const [label, setlableEntries]=useState([]);
      // Hold Average & Delete Value
      const [average, setAverage]=useState(0);
-     const [excludedata,setExclude]=useState(0);
-     //Prevous data hold before update;
-     const[tempdata,setTempdata]=useState(0);
-     const[templabel, setTemplabel]=useState(0);
+     const [excludedata,setExclude]=useState([]);
+   
+    // object of data
+    const[excluded, setexcludeD]=useState([{Data:"",Label:""}])
+   
+   
      //Tooltip
      const [tooltipOpen, setTooltipOpen] = useState(false);
 
      const toggle = () => setTooltipOpen(!tooltipOpen);
- 
-
-
-
-
      const handleDelete = (event) => {
-        setTempdata(chartEntries);
-        setTemplabel(label);
          if(event.length>0){
             let index=event[0]._index;
-            setExclude(chartEntries[index]);
+            setExclude([...excludedata,
+            chartEntries[index]
+        ]);
+        let newList = [...excluded, ...[{Data: chartEntries[index], Label: label[index]}]]
+            setexcludeD(newList.flat());
             let newEntries = chartEntries.filter((entry, entryIindex) => entryIindex !== index);
             setchartEntries(newEntries)
             let newlableEntries=label.filter((entry,labalentry)=>labalentry!==index);
             setlableEntries(newlableEntries);
-            setAverage(find_average(newEntries));
+            setAverage(find_average(newEntries).toFixed(4));
          }
 
      }
@@ -101,10 +101,15 @@ function LineChart(){
        }
    }
 const upPrevData=(event)=>{
-    setExclude(0);
-    setlableEntries(templabel);
-    setchartEntries(tempdata);
-    setAverage(find_average(tempdata));
+    let entry=event.target.innerHTML;
+    let x=entry.split(" ");
+    setlableEntries([...label,
+        x[1]
+    ]);
+    const FilterData = excluded.filter((item) => item.Label !== x[1]);
+    setexcludeD(FilterData);
+    setchartEntries([...chartEntries,parseInt(x[0])]);
+    setAverage(find_average(chartEntries).toFixed(4));
 }
  useEffect(()=>{ 
     if(chartEntries.length > 0) {
@@ -112,7 +117,7 @@ const upPrevData=(event)=>{
     }else{
         setlableEntries(labal);
         setchartEntries(data);
-        setAverage(find_average(data));
+        setAverage(find_average(data).toFixed(4));
     }
  },[chartEntries]
 
@@ -124,11 +129,19 @@ const upPrevData=(event)=>{
                <Col style={{width:"500px"}} >
             <Line data={chartdata} options={options}  onElementsClick={(e) =>handleDelete(e)}/></Col>
             <Col justify-center style={{padding:"20px"}}>
-           <h5 onClick={()=>upPrevData()} id="ExcludeDataMsg">Excluding Data:{excludedata}</h5>
+           <h5  id="ExcludeDataMsg">Excluding Data:</h5>
+           {excluded.map((item,i)=>(
+              <>
+              <h6 onClick={(e)=>upPrevData(e)}>{item.Data} {item.Label}</h6>
+                    </>  
+            ))}
            <Tooltip placement="left" isOpen={tooltipOpen} target="ExcludeDataMsg" toggle={toggle}>
         Click me to Enclude this Data
       </Tooltip>
             <h5>Average of Data:{average}</h5>
+
+
+           
             </Col> 
             </Row> 
         </Container>
